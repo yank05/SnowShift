f2f5e263ac7396c22ce437f5b1dbc910581f465f
yank05 <koenigyannik@web.de>
2024-02-07 13:53:44 +0100

    +

--- quiz.js
+++ quiz.js
@@ -3,9 +3,10 @@
 window.addEventListener("load", handleLoad);
 
 let values = []; 
+let correctAnswers = ['2.92', '70%', '175']; 
+let result; 
 
 function handleLoad() {
-    console.log("ggg");
 
     let div = document.getElementById("div");
     let button = document.createElement("button"); 
@@ -43,6 +44,11 @@ function removeFrontPage() {
     let button = document.getElementById("startButton");
     div.removeChild(button);
 
+    let banner1 = document.getElementById("banner1");
+    banner1.remove(); 
+    let banner2 = document.getElementById("banner2");
+    banner2.remove(); 
+
 
   for (let index = 0; index < 3; index++) {
     let div = document.getElementById("text" + index);
@@ -53,6 +59,7 @@ function removeFrontPage() {
     let h = document.getElementById("header" + index);
     h.remove();
  }
+
 
 }
 
@@ -68,18 +75,22 @@ function buildQuizPageGeneral() {
     }
     let div = document.getElementById("div1"); 
 
-    let video = document.createElement("video");
-    video.setAttribute("id", "video"); 
-    video.setAttribute("src", "../SnowShift/animations/animation1/grafik01.mp4");
-    video.setAttribute("type", "video/mp4"); 
-    video.setAttribute("autoplay", true); 
-    video.setAttribute("alt", "animation showing two different snow masses");
-    div.appendChild(video);
+    if (document.getElementById("imgBox")) {
+        let imgDiv = document.getElementById("imgBox");
+        let img = document.createElement("img");
+        img.setAttribute("id", "img"); 
+        imgDiv.appendChild(img); 
+    } else {
+        let imgDiv = document.getElementById("text");
+        imgDiv.setAttribute("id", "imgBox");
+        document.body.insertBefore(imgBox, div1);
 
-    let neonBeam = document.createElement("div");
-    neonBeam.setAttribute("id", "neonBeam"); 
-    div.appendChild(neonBeam); 
-
+        let img = document.createElement("img");
+        img.setAttribute("id", "img"); 
+        imgDiv.appendChild(img); 
+    
+    }
+    
     let questionbox = document.createElement("div");
     questionbox.setAttribute("class", "introduction-container");
     questionbox.setAttribute("id", "questionbox");
@@ -88,30 +99,24 @@ function buildQuizPageGeneral() {
 
     let div2 = document.createElement("div");
     div2.setAttribute("id", "div2");
-    document.body.appendChild(div2); 
+    document.body.insertBefore(div2, div1); 
 
     let inputElement = document.createElement("input");
 
     inputElement.type = 'number'; 
     inputElement.setAttribute("id", "inputElement");
-    div2.appendChild(inputElement); 
-
-    inputElement.addEventListener('input', function() {
-    var enteredNumber = inputElement.value;
-    values.push(enteredNumber); 
-    console.log(values); 
-    });
+    div1.appendChild(inputElement); 
 
     let button = document.createElement("button"); 
     button.setAttribute("id", "goOnButton"); 
     button.innerHTML = "Weiter";
-    document.body.appendChild(button);
+    div1.appendChild(button);
 }
 
 function buildQuizPage1() {
-    let video = document.getElementById("video");
-    video.setAttribute("src", "../SnowShift/animations/animation1/grafik01.mp4");
-    video.setAttribute("alt", "animation showing two different snow masses");
+    let img = document.getElementById("img");
+    img.setAttribute("src", "../SnowShift/animations/animation1/grafik01.gif");
+    img.setAttribute("alt", "animation showing two different snow masses");
 
     let question = document.createElement("p");
     question.innerHTML = "1960 fielen auf dem Feldberg in einem Winter insgesamt 6,06 Meter Schnee. <br> <br> Wie viel Meter sind es heute?"
@@ -137,50 +142,39 @@ function buildQuizPage1() {
 
 function NextQuestion2() {
     let input = document.getElementById("inputElement");
+
     if (input.value == 0) {
         alert("Gebe eine Antwort ein!");
+    } else if (input.value > 6 || input.value < 0) {
+        alert("Diese Antwort ergibt keinen Sinn!"); 
     } else {
+        var enteredNumber = inputElement.value;
+        values.push(enteredNumber); 
         deletePage();
         buildQuizPage2();
     }
     }
 
 function deletePage() {
+
    let div1 = document.getElementById("div1");
    div1.remove();
 
    let div2 = document.getElementById("div2");
    div2.remove(); 
-
-   let button = document.getElementById("goOnButton");
-   button.remove(); 
 }
 
 function buildQuizPage2() {
     buildQuizPageGeneral(); 
 
-    let video = document.getElementById("video");
-    video.setAttribute("src", "../SnowShift/animations/animation2/grafik02.mp4");
-    video.setAttribute("alt", "animation of a decreasing water mass");
+    let img = document.getElementById("img");
+    img.setAttribute("src", "../SnowShift/animations/animation2/grafik02.gif");
+    img.setAttribute("alt", "animation of a decreasing water mass");
 
     let question = document.createElement("p");
     question.innerHTML = "Zur Schneeerzeugung mit Schneekanonen halten Skigebietsbetreiber Wasser in Beschneiungsteichen zurück. <br> Dieses Wasser kommt  nicht im Tal an. <br> <br> Um bis zu wie viel Prozent hat die Wassermenge in Flüssen in den französischen Alpen abgenommen, seit es Schneekanonen gibt?"
     let questionbox = document.getElementById("questionbox"); 
     questionbox.appendChild(question); 
-
-    let div2 = document.getElementById("div2")
-
-    let schrift1 = document.createElement("p")
-    schrift1.innerHTML = "1960";
-    schrift1.setAttribute ("id", "timecall1");
-    schrift1.style.opacity = "0"; 
-    div2.appendChild(schrift1);
-
-    let schrift2 = document.createElement("p")
-    schrift2.innerHTML = "heute";
-    schrift2.setAttribute ("id", "timecall2");
-    schrift2.style.opacity = "0"; 
-    div2.appendChild(schrift2);
     
     let button = document.getElementById("goOnButton"); 
     button.addEventListener("click", NextQuestion3);
@@ -190,7 +184,11 @@ function NextQuestion3() {
     let input = document.getElementById("inputElement");
     if (input.value == 0) {
         alert("Gebe eine Antwort ein!");
+    } else if (input.value > 100 || input.value < 0) {
+        alert("Diese Antwort ergibt keinen Sinn!"); 
     } else {
+        var enteredNumber = inputElement.value;
+        values.push(enteredNumber); 
         deletePage();
         buildQuizPage3();
     }
@@ -199,26 +197,111 @@ function NextQuestion3() {
 function buildQuizPage3() {
     buildQuizPageGeneral(); 
     
-    let video = document.getElementById("video");
-    video.setAttribute("src", "../SnowShift/animations/animation3/grafik03.mp4");
-    video.setAttribute("alt", "animation of dots represtenting ski arenas disappearing");
+    let img = document.getElementById("img");
+    img.setAttribute("src", "../SnowShift/animations/animation3/grafik03.gif");
+    img.setAttribute("alt", "animation of dots represtenting ski arenas disappearing");
     
     let question = document.createElement("p");
-    question.innerHTML = "Um Schneekanonen zu betreiben, halten Skigebietsbetreiber Wasser in Beschneiungsteichen zurück. <br> Dieses Wasser kommt  nicht im Tal an. <br> <br> Um bis zu wie viel Prozent hat die Wassermenge in Flüssen in den französischen Alpen abgenommen, seit es Schneekanonen gibt?"
+    question.innerHTML = "2023 gab es in Deutschland etwa 700 Skigebiete. <br> Wie viele davon sind in den nächsten Jahren direkt von einer Schließung bedroht?"
     let questionbox = document.getElementById("questionbox"); 
     questionbox.appendChild(question); 
 
-    let neonBeam = document.getElementById("neonBeam");
-    neonBeam.style.left= "33%"; 
-
     let div2 = document.getElementById("div2")
-
-    let schrift1 = document.createElement("p")
-    schrift1.innerHTML = "1960";
-    schrift1.setAttribute ("id", "timecall1");
-    schrift1.style.opacity = "0"; 
-    div2.appendChild(schrift1);
     
     let button = document.getElementById("goOnButton"); 
-    button.addEventListener("click", NextQuestion3);
+    button.addEventListener("click", finals);
     }
+
+function finals() {
+    let input = document.getElementById("inputElement");
+    if (input.value == 0) {
+        alert("Gebe eine Antwort ein!");
+    } else if (input.value > 700 || input.value < 0) {
+        alert("Diese Antwort ergibt keinen Sinn!"); 
+    } else {
+        var enteredNumber = inputElement.value;
+        values.push(enteredNumber); 
+        deletePage();
+        let img = document.getElementById("imgBox");
+        img.remove(); 
+        buildFinalPage();
+    }
+
+    }
+
+function buildFinalPage() {
+    calculateResult(); 
+
+    let div1 = document.createElement("div");
+    div1.setAttribute("id", "resultBox"); 
+    document.body.appendChild(div1);
+
+    let resultText = document.createElement("p");
+    resultText.setAttribute("id", "headerResult"); 
+    resultText.innerHTML = "Dein Ergebnis";
+    div1.appendChild(resultText); 
+
+    let resultPercent = document.createElement("h1");
+    resultPercent.innerHTML = result + "%";
+    div1.appendChild(resultPercent); 
+
+    let explanation = document.createElement("p");
+    explanation.setAttribute("id", "explanation");
+
+    if (result > 0) {
+        explanation.innerHTML = "Deine Werte weichen um " + result + "% vom Ergebnis ab.<br/>Du überschätzt die Folgen der Klimakrise auf das Skifahren."
+    }
+    else {
+        explanation.innerHTML = "Deine Werte weichen um " + result + "% vom Ergebnis ab.<br/>Du unterschätzt die Folgen der Klimakrise auf das Skifahren."
+    }
+    div1.appendChild(explanation); 
+     
+    for (let index = 1; index < 4; index++) {
+        let box = document.createElement("details");
+        div1.appendChild(box);
+        let summary = document.createElement("summary");
+        summary.innerHTML = "Antwort auf Frage " + index; 
+        box.appendChild(summary);
+        
+        let p = document.createElement("p");
+        
+        console.log(index); 
+        if (index == 1) {
+            p.innerHTML = "Die korrekte Antwort bei Frage 1 ist 2.92 Meter. Die Menge an Schnee, die insgesamt auf dem Feldberg fällt hat sich damit in den letzten 40 Jahren mehr als halbiert."
+        } else if (index == 2) {
+            p.innerHTML = "Die korrekte Antwort bei Frage 2 ist bis zu 70%. Im Winter wird für die Beschneiung so viel Wasser verbraucht wie eine Stadt mit 1,5 Mio. Menschen in einem Jahr benötigt. <br/> Das Wasser wird " + 
+            "im Winter zurückgehalten und gespeichert, es fehlt dann in den Flüssen. Das führt dazu, dass teilweise bis zu 70% weniger Wasser im Tal ankommt. Hier kommt es in Frankreich bereits zu Konflikten zwischen Skigebiets" +
+            "betreibern und Bauern."
+        } else if (index == 3) {
+            p.innerHTML = "Die korrekte Antwort bei Frage 3 ist bis zu 175. Mindestens 25% der Skigebiete in Deutschland, insbesondere in niedrigeren Lagen werden in den kommenden Jahren auch mit künstlicher Beschneiung nur schwer " +
+            "überleben, weil es einfach zu warm wird."
+        } else {
+            alert("Fehler!");  
+        }
+        box.appendChild(p)
+    }
+
+
+
+
+}
+
+function calculateResult() {
+    let totalDeviationPercent = 0;
+
+    for (let i = 0; i < correctAnswers.length; i++) {
+        let userAnswer = parseFloat(values[i]);
+        let correctAnswer = parseFloat(correctAnswers[i]);
+
+
+        let deviation = ((userAnswer - correctAnswer) / correctAnswer) * 100;
+
+        totalDeviationPercent += deviation;
+    }
+
+    let averageDeviationPercent = totalDeviationPercent / correctAnswers.length;
+
+    result = Math.round(averageDeviationPercent * 100) / 100;
+}
+
+    
